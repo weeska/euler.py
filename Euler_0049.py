2888a7f691c4252cd5f4b17a2dfff3991ce097d2
Philipp B <ph.boehnke@gmail.com>
2015-04-02 21:41:36 +0200

    Euler 49

---
+++ Euler_0049.py
@@ -0,0 +1,32 @@
+from stuff.primes import is_prime_naive
+
+def result(): # makes retun easier
+	for a in range(1000, 10000):
+
+		if a == 1487:
+			continue
+
+		if not is_prime_naive(a):
+			continue
+
+		aDigits = set(str(a))
+
+		for i in range(1, 9001):
+			b = a + i
+			c = b + i
+
+			if c > 9999:
+				break
+
+			bDigits = set(str(b))
+			cDigits = set(str(c))
+
+			if not ( aDigits == bDigits == cDigits):
+				continue
+
+			if not (is_prime_naive(b) and is_prime_naive(c)):
+				continue
+
+			return "%s%s%s" % (a, b, c)
+
+print(result())
